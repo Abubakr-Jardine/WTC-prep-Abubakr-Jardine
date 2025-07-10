6f1934733d9baa915baff35fe109fe05ee1f3479
Abubakr-Jardine <abubakr.jardine172@gmail.com>
2025-07-10 14:37:47 +0000

    Thu, Jul 10, 2025, 4:37 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,11 +1,10 @@
-def mediatype(file):
-    list_of_types = ["gif","jpg","jpeg","png","pdf","txt","zip"]
-    file = file.strip().lower()
+file = input()
+list_of_types = ["gif","jpg","jpeg","png","pdf","txt","zip"]
+file = file.strip().lower()
 
-    for type in list_of_types:
-        if type in file[-5:]:
-            print(f"image/{type}")
+for type in list_of_types:
+    if type in file[-5:]:
+        print(f"image/{type}")
 
 
-mediatype("jnvdksajvnijvfi.jpg")
 
