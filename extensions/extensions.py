a42e22c01959d47d6a17e0d5b2aa0db2163e716b
Abubakr-Jardine <abubakr.jardine172@gmail.com>
2025-07-10 13:56:02 +0000

    Thu, Jul 10, 2025, 3:56 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,4 +2,7 @@ def mediatype(file):
     list_of_types = [".gif",".jpg",".jpeg",".png",".pdf",".txt",".zip"]
     file = file.strip().lower()
 
-    
+    if list_of_types in file[0:5:-1]:
+        print("yes")
+
+mediatype("jnvdksajvnijvfi.jpg")
